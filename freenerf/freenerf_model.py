77fe5568c212849a4823a5db2439f79abdbc84f7
FedericoPaolucci <s1116751@studenti.univpm.it>
2024-07-09 15:57:53 +0200

    step

--- freenerf/freenerf_model.py
+++ freenerf/freenerf_model.py
@@ -34,7 +34,7 @@ class FreeNeRFModelConfig(VanillaModelConfig):
     """Number of frequencies for positional encoding"""
     direction_encoding_num_frequencies: int = 4
     """Number of frequencies for directional encoding"""
-    T: int = 30000
+    T: int = 43945
     """Number of training steps (must equal to max-num-iterations)"""
     loss_coefficients: Dict[str, float] = to_immutable_dict({"rgb_loss_coarse": 1.0, "rgb_loss_fine": 1.0, "occ_reg_loss": 0.01})
     """loss coefficient and Occlusion reg loss molt"""
