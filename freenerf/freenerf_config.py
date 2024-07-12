7ab0b0a19863be241c428078b33294739dba3455
Elisa <pace.elisa1@gmail.com>
2024-07-12 23:16:22 +0200

    UPDATE freenerf_config.py add attributes eval_mode="filename" for split evaluation based on image_filename

--- freenerf/freenerf_config.py
+++ freenerf/freenerf_config.py
@@ -30,7 +30,7 @@ freenerf_method = MethodSpecification(
         mixed_precision=True, #precisione mista (riduce utilizzo di memoria)
         pipeline=VanillaPipelineConfig( 
             datamanager=VanillaDataManagerConfig(
-                dataparser=NerfstudioDataParserConfig(), # datamanager e dataparser di nerfstudio
+                dataparser=NerfstudioDataParserConfig( eval_mode="filename"), # datamanager e dataparser di nerfstudio
                 train_num_rays_per_batch=4096,
                 eval_num_rays_per_batch=4096,
             ),
